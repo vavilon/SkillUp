0027a3e6c0bd4e3493653c88c5f0b14ab50877c3
VintorezVS <vintorezvs@gmail.com>
2015-05-15 23:57:00 +0300

    Добавлены записи в БД

--- test_db.js
+++ test_db.js
@@ -1,20 +1,16 @@
-var pg = require('pg');
-
-var conString = "postgres://skillupserv:1111@localhost/skillup";
-
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync(__dirname + '/models/skills.json', 'utf8'));
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host     : 'localhost',
+    user     : 'skillupserv',
+    password : '1111',
+    database : 'skillup'
   }
-  client.query("INSERT INTO skills VALUES (uuid_generate_v4(), 'bla bal', ARRAY[uuid_generate_v4(), uuid_generate_v4()]);",
-      function(err, result) {
-    //call `done()` to release the client back to the pool
-    done(client);
-
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result);
-    //output: 1
-  });
 });
+
+knex.insert(obj).into('skills').catch(function(error) {
+  console.error(error);
+});
+knex.destroy();
