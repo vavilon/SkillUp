564a0081f91ea82dddce422ae82756fc46c9ae57
Зинкевич Алексей <alexzinkevych@yandex.ua>
2015-07-18 17:21:35 +0300

    Нельзя решить неподтвержденное

--- controllers/tasks/solve.js
+++ controllers/tasks/solve.js
@@ -7,12 +7,16 @@ module.exports = function(knex) {
                 res.end();
                 return;
             }
-            knex('solutions').returning('id').insert({
-                task_id: req.body.task_id,
-                user_id: req.user.id,
-                content: req.body.content
-            }).then(function (id) {
-                knex('tasks').where('id', '=', req.body.task_id).select('exp').then(function(rows) {
+            knex('tasks').where('id', '=', req.body.task_id).select('exp', 'is_approved').then(function(rows) {
+                if (!rows[0].is_approved) {
+                    res.end();
+                    return;
+                }
+                knex('solutions').returning('id').insert({
+                    task_id: req.body.task_id,
+                    user_id: req.user.id,
+                    content: req.body.content
+                }).then(function (id) {
                     knex.raw("UPDATE users SET tasks_done = array_append(tasks_done, '" + req.body.task_id + "')"
                         + ", exp = exp - " + rows[0].exp  + ", tasks_received = array_remove(tasks_received, '" +
                         req.body.task_id + "')" + " WHERE id = '" + req.user.id + "';").then(function() {
