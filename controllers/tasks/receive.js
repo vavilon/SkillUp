ff98c34a79e174ad7e6d6be9e187d6321b971ab2
Зинкевич Алексей <alexzinkevych@yandex.ua>
2015-07-20 15:04:43 +0300

    Нельзя взять неподтвержденное

--- controllers/tasks/receive.js
+++ controllers/tasks/receive.js
@@ -7,26 +7,35 @@ module.exports = function(knex, updateArray) {
                 res.end();
                 return;
             }
-            knex('users').where('id', '=', req.user.id).andWhere('tasks_received', '@>', [req.body.task_id]).select('id')
-                .then(function (rows) {
-                    var operation = rows.length === 0 ? 'append' : 'remove';
-                    updateArray('users', 'tasks_received', req.user.id, operation, req.body.task_id, function (err, result) {
-                        if (err) {
-                            res.end();
-                            return console.error('error running query', err);
-                        }
-                        updateArray('tasks', 'participants', req.body.task_id, operation, req.user.id, function (err, result) {
+            knex('tasks').where('id', '=', req.body.task_id).select('is_approved').then(function(tasks) {
+                if (!tasks[0].is_approved) {
+                    res.end();
+                    return;
+                }
+                knex('users').where('id', '=', req.user.id).andWhere('tasks_received', '@>', [req.body.task_id]).select('id')
+                    .then(function (rows) {
+                        var operation = rows.length === 0 ? 'append' : 'remove';
+                        updateArray('users', 'tasks_received', req.user.id, operation, req.body.task_id, function (err, result) {
                             if (err) {
                                 res.end();
                                 return console.error('error running query', err);
                             }
-                            res.end('ok');
+                            updateArray('tasks', 'participants', req.body.task_id, operation, req.user.id, function (err, result) {
+                                if (err) {
+                                    res.end();
+                                    return console.error('error running query', err);
+                                }
+                                res.end('ok');
+                            });
                         });
+                    }).catch(function (error) {
+                        console.log(error);
+                        res.end();
                     });
-                }).catch(function (error) {
-                    console.log(error);
-                    res.end();
-                });
+            }).catch(function (error) {
+                console.log(error);
+                res.end();
+            });
         } else res.end();
     };
 };
