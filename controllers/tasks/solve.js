9e2f25fbfcf3b40d99cffdc2cbe8ee15bf550a17
Зинкевич Алексей <alexzinkevych@yandex.ua>
2015-12-22 18:59:14 +0200

    Контроллер tasks/solve изменен в соответствии с новой структурой БД

--- controllers/tasks/solve.js
+++ controllers/tasks/solve.js
@@ -1,34 +1,40 @@
 
 module.exports = function(knex) {
     return function (req, res, next) {
-        if (req.isAuthenticated()) {
-            //Нельзя решить уже решенное тобой или созданное тобой задание
-            if (req.user.tasks_done && req.user.tasks_done.indexOf(req.body.task_id) !== -1
-                || req.user.tasks_created && req.user.tasks_created.indexOf(req.body.task_id) !== -1) {
-                res.end();
-            }
-            else knex('tasks').where('id', '=', req.body.task_id).select('exp', 'is_approved').then(function(tasks) {
-                if (!tasks[0].is_approved) { //Нельзя решить неподтвержденное или подтвержденное некорректное
-                    res.end();
-                }
-                else knex('solutions').returning('id').insert({ //Добавляем решение в бд
-                    task_id: req.body.task_id,
-                    user_id: req.user.id,
-                    content: req.body.content
-                }).then(function (id) {
-                    //Добавляем решающему id задания в tasks_done, снимаем експу и убираем id задания из tasks_received
-                    knex.raw("UPDATE users SET tasks_done = array_append(tasks_done, '" + req.body.task_id + "')"
-                        + ", exp = exp - " + tasks[0].exp  + ", tasks_received = array_remove(tasks_received, '" +
-                        req.body.task_id + "')" + " WHERE id = '" + req.user.id + "';").then(function() {
-                        res.end('ok');
+        if (req.isAuthenticated()) { //Возьмем задание и метаданные для решающего
+            knex('tasks').where('id', '=', req.body.task_id).select('exp', 'is_approved', 'done', 'created',
+                knex.raw('task_id IS NOT NULL as meta_exists'))
+                .leftJoin('tasks_meta as tm', {'tasks.id': 'tm.task_id', 'tm.user_id': req.user.id}).then(function(task) {
+                    task = task[0];
+                    //Нельзя решить неподтвержденное или подтвержденное некорректное, решенное тобой или созданное тобой задание
+                    if (!task.is_approved || task.done !== null || task.created !== null) {
+                        res.end();
+                    }
+                    else knex('solutions').insert({ //Добавляем решение в бд
+                        task_id: req.body.task_id,
+                        user_id: req.user.id,
+                        content: req.body.content
+                    }).then(function () {
+                        var query = knex('tasks_meta'); //Запишем в tasks_meta.done значение true для решающего
+                        var meta = {done: true};
+                        //Если запись для текущего проверяющего есть - update
+                        if (task.meta_exists) query.update(meta).where('task_id', req.body.task_id).andWhere('user_id', req.user.id);
+                        else { //иначе - insert
+                            meta.task_id = req.body.task_id;
+                            meta.user_id = req.user.id;
+                            query.insert(meta);
+                        }
+                        //Добавляем решающему id задания в tasks_done, снимаем експу и убираем id задания из tasks_received
+                        query.then(function() {
+                            res.end('ok');
+                        }).catch(function (error) {
+                            console.log(error);
+                            res.end();
+                        });
                     }).catch(function (error) {
                         console.log(error);
                         res.end();
                     });
-                }).catch(function (error) {
-                    console.log(error);
-                    res.end();
-                });
             }).catch(function (error) {
                 console.log(error);
                 res.end();
