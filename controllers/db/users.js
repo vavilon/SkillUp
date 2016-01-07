1bcec2e22b735e75497aaab9f0b3eaf99b34d471
Зинкевич Алексей <alexzinkevych@yandex.ua>
2016-01-07 16:55:45 +0200

    Добавлены лимиты

--- controllers/db/users.js
+++ controllers/db/users.js
@@ -4,7 +4,7 @@ module.exports = function (knex, req, res, next) {
     if (req.body.id) q.where('users.id', req.body.id);
     else if (req.body.ids) q.whereIn('users.id', req.body.ids);
     q.leftJoin('user_skills', 'id', '=', 'user_id').select(knex.raw("array_agg((skill_id, count, need)) AS skills"))
-        .groupBy('id').limit(20).offset(req.body.offset || 0);
+        .groupBy('id').limit(req.body.limit > 100 ? 20 : req.body.limit).offset(req.body.offset || 0);
 
     q.then(function(rows) {
         if (!rows) res.end();
