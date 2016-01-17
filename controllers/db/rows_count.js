d9b6fbb4d79d749a3abe1a8519a583b31b5b1f1c
Сергей <vintorezvs@gmail.com>
2016-01-17 21:11:45 +0200

    Убрана проверка из-за которой пользователь без прав админа не мог получить кол-во строк таблицы

--- controllers/db/rows_count.js
+++ controllers/db/rows_count.js
@@ -1,15 +1,10 @@
 module.exports = function(knex) {
     return function (req, res, next) {
-        if (req.user.role !== 'пользователь') {
-            knex(req.params.table).count('id').then(function (data) {
-                res.end(JSON.stringify(data[0].count));
-            }).catch(function (error) {
-                console.log(error);
-                res.error(error);
-            });
-        } else {
-            console.log('Попытка получить доступ без прав админитратора');
-            res.error('Попытка получить доступ без прав админитратора');
-        }
+        knex(req.params.table).count('id').then(function (data) {
+            res.end(JSON.stringify(data[0].count));
+        }).catch(function (error) {
+            console.log(error);
+            res.error(error);
+        });
     }
 };
