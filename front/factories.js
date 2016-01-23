28a4ad29c84976e9525657ac2b5ad75805928cd6
Сергей <vintorezvs@gmail.com>
2016-01-23 16:28:12 +0200

    Создана дфабрика для склонения существительного после числителя

--- front/factories.js
+++ front/factories.js
@@ -297,3 +297,24 @@ app.factory('editNeed', function($http, $rootScope) {
         });
     };
 });
+
+app.factory('getEndingVariant', function() {
+    return function(number, variants) {
+        var ending, i;
+        number = number % 100;
+        if (number >= 11 && number <= 19) {
+            ending = variants[2];
+        }
+        else {
+            i = number % 10;
+            switch (i) {
+                case (1): ending = variants[0]; break;
+                case (2):
+                case (3):
+                case (4): ending = variants[1]; break;
+                default: ending = variants[2];
+            }
+        }
+        return ending;
+    };
+});
