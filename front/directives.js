e0f291ba8c5b07a8656e4a70d9833e99c1ff5c45
Сергей <vintorezvs@gmail.com>
2016-01-26 15:59:03 +0200

    Добавлена директива для комментариев

--- front/directives.js
+++ front/directives.js
@@ -242,9 +242,11 @@ app.directive('likeButton', function() {
             likes: '=',
             liked: '=',
             like: '=?',
-            disabled: '=?'
+            disabled: '=?',
+            size: '@'
         },
         controller: function($scope, $http, loadLoggedUser) {
+            if($scope.size == "" || $scope.size == undefined) $scope.size = 24;
             if ($scope.like === undefined) $scope.like = function () {
                 $scope.liked = !$scope.liked;
                 $scope.liked ? $scope.likes++ : $scope.likes--;
@@ -427,33 +429,63 @@ app.directive("highlightSkill", function ($rootScope, $timeout) {
     };
 });
 
-app.directive('comments', function($rootScope, $http) {
+app.directive('comments', function($rootScope, $http, $mdToast) {
     return {
         restrict: 'E',
         templateUrl: '/dist/templates/comments.html',
         scope: {
-            srcID: '@',
+            idSrc: '=',
             callback: '=?'
         },
         link: function (scope, element, attrs) {
-            scope.loggedUser = $rootScope.loggedUser;
-        },
-        controller: function($scope) {
-            $scope.src = {};
+            scope.el = document.getElementById('comment');
 
-            $scope.el = document.getElementById('comment');
-
-            var oldCommentHeight = $scope.el.offsetHeight;
-            $scope.$watch('src.comment', function (newValue) {
-                if (newValue && oldCommentHeight && $scope.el.offsetHeight != oldCommentHeight) {
-                    $scope.el.parentNode.parentNode.style.height =  $scope.el.offsetHeight + 9 + 'px';
-                    oldCommentHeight = $scope.el.offsetHeight;
+            var oldCommentHeight = scope.el.offsetHeight;
+            scope.$watch('src.comment', function (newValue) {
+                if (newValue && oldCommentHeight && scope.el.offsetHeight != oldCommentHeight) {
+                    scope.el.parentNode.parentNode.style.height =  scope.el.offsetHeight + 9 + 'px';
+                    oldCommentHeight = scope.el.offsetHeight;
                 }
             });
+        },
+        controller: function($scope) {
+            $scope.loggedUser = $rootScope.loggedUser;
+            $scope.src = {};
+            $scope.src.comments = [];
+
+            updateComments($scope.idSrc);
+
             $scope.src.addComment = function () {
-                //TODO: запрос на добавление коммента
+                $http.post('/add_comment', {src: $scope.idSrc, content: $scope.src.comment}).success(function (data) {
+                    if (data == 'added') {
+                        $scope.src.comment = '';
+                        updateComments($scope.idSrc);
+                    } else {
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent('При добавлении комментария произошла ошибка')
+                                .hideDelay(2000)
+                        );
+                    }
+                });
                 $scope.callback && $scope.callback($scope.src.comment);
             };
+
+            function updateComments(srcID) {
+                $http.post('/db/comments', {src: srcID}).success(function (rows) {
+                    $scope.src.comments = rows;
+                    var date = new Date();
+                    var timeElapsed;
+                    for (var index in $scope.src.comments) {
+                        timeElapsed = new Date(date - new Date($scope.src.comments[index].date_created)).getTime();
+                        if (timeElapsed >= 2592000000) $scope.src.comments[index].timeElapsed = new Date(timeElapsed).toLocaleDateString();
+                        else if (timeElapsed >= 86400000) $scope.src.comments[index].timeElapsed = Math.floor(timeElapsed / 86400000) + ' д.';
+                        else if (timeElapsed >= 3600000) $scope.src.comments[index].timeElapsed = Math.floor(timeElapsed / 3600000) + ' ч.';
+                        else if (timeElapsed >= 60000) $scope.src.comments[index].timeElapsed = Math.floor(timeElapsed / 60000) + ' м.';
+                        else $scope.src.comments[index].timeElapsed = Math.floor(timeElapsed / 1000) + ' с.';
+                    }
+                });
+            }
         }
     }
 });
